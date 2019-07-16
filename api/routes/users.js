5ee3ca36d16dd4f10b52f8759f8b8e7314b51eab
Prashant Jain <prashant.jain@squareyards.co.in>
2019-07-16 23:24:11 +0530

    new files

---
+++ api/routes/users.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+router.post("/signup", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Mail Exist"
+        })
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
+            });
+          } else {
+            const user = new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            });
+            user
+              .save()
+              .then(result => {
+                console.log(result)
+                res.status(201).json({
+                  message: "Created User successfully",
+                });
+              })
+              .catch(err => {
+                res.status(500).json({
+                  error: err
+                });
+              });
+          }
+        });
+      }
+    })
+
+});
+
+router.delete('/:userId', (req, res, next) => {
+  const id = req.params.userId
+  User.remove({
+    _id: id
+  })
+    .then(result => {
+      res.status(200).json({
+        message: 'User deleted',
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+})
+module.exports = router;
