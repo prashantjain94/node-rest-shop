de6b7c6f07abd633c219eb3f790add0c885bec12
Prashant Jain <prashant.jain@squareyards.co.in>
2019-08-16 17:43:38 +0530

    api completed

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const UserController = require('../controllers/user')
+const UserController = require('../controllers/users')
 const checkAuth = require('../middleware/check-auth')
 router.post("/signup", UserController.user_signup);
 router.post('/login', UserController.user_login)
